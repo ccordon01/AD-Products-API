1e63bc989817a7b2f11db24602b5d53656e7c8d0
Carlos Cordon <carlos.edu.cordon@gmail.com>
2024-09-09 01:51:43 -0600

    docs: Setting up swagger.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,14 @@ async function bootstrap() {
     type: VersioningType.URI,
   });
 
+  const config = new DocumentBuilder()
+    .setTitle('Products API')
+    .setDescription('Products API for managing a collection of products.')
+    .setVersion('1.0')
+    .addTag('Public')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
   await app.listen(process.env.API_PORT ?? 3000);
 }
 bootstrap();
--- src/products/controllers/public-products.controller.ts
+++ src/products/controllers/public-products.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Post } from '@nestjs/common';
 import { ProductsService } from '../products.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Public')
 @Controller('products')
 export class PublicProductsController {
   constructor(private productsService: ProductsService) {}
